c44bc36d55dab8710ec79d8785cf3b1ee0758c84
Sunitakamble <sunitakam147@gmail.com>
2024-06-07 19:07:56 +0530

    Add files via upload

---
+++ addfunction.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 14 19:40:46 2024
+
+@author: DELL
+"""
+
+def add (x,y):
+    return x+y
---
+++ subfunction.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 14 20:06:25 2024
+
+@author: DELL
+"""
+
+def sub(a,b):
+   return a-b
